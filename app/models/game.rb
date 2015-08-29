427adcc4cada22707ae382fb9aa2eb86d801d072
Zequez <zequez@gmail.com>
2015-08-29 06:18:45 -0300

    Started Game#name filter and filter infrastructure

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,37 @@
 class Game < ActiveRecord::Base
+  default_scope { select('games.*') }
+
+  def self.register_filter(filter_name, proc_block)
+    (@@filters ||= []) << filter_name
+    scope :"filter_by_#{filter_name}", proc_block
+  end
+
+  def self.register_sort(sort_name, proc_block)
+    (@sorts ||= []) << sort_name
+    scope :"sort_by_#{sort_name}", proc_block
+  end
+
+  register_filter :name, (lambda do |filter|
+    condition = sanitize_sql_array(['LOWER(name) = ?', filter[:value].downcase])
+    filter_and_or_highlight(:name, filter, condition)
+  end)
+
+  # Condition and name should be sanitized!
+  def self.filter_and_or_highlight(name, filter, condition)
+    scope = all
+
+    if filter[:filter]
+      scope = scope.where(condition)
+      if filter[:highlight]
+        condition = '1' # Avoid running the condition again
+      end
+    end
+
+    if filter[:highlight]
+      hl_column = "#{condition} AS hl_#{name}"
+      scope = scope.select hl_column
+    end
+
+    scope
+  end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,4 +3,54 @@ describe Game, type: :model do
   it { is_expected.to respond_to :name }
   it { is_expected.to respond_to :created_at }
   it { is_expected.to respond_to :updated_at }
+
+  describe '#filter_by_name' do
+    it 'should return only the games with the same name' do
+      create :game, name: 'Potato'
+      g2 = create :game, name: 'Galaxy'
+
+      games = Game.filter_by_name(value: 'Galaxy', filter: true)
+      expect(games).to match_array [g2]
+    end
+
+    it 'should be able to highlight instead of filtering' do
+      g1 = create :game, name: 'Potato'
+      g2 = create :game, name: 'Galaxy'
+
+      games = Game.filter_by_name(value: 'Galaxy', highlight: true)
+
+      expect(games).to match_array [g1, g2]
+      highlighted = games.map(&:hl_name)
+
+      expect(highlighted).to match_array [1, 0]
+    end
+
+    it 'should be able to highlight AND filter' do
+      create :game, name: 'Potato'
+      g2 = create :game, name: 'Galaxy'
+
+      games = Game.filter_by_name(value: 'Galaxy', filter: true, highlight: true)
+
+      expect(games).to match_array [g2]
+      highlighted = games.map(&:hl_name)
+
+      expect(highlighted).to match_array [1]
+    end
+
+    it 'should match partial names' do
+
+    end
+
+    it 'should be case insensitive' do
+      create :game, name: 'Potato'
+      g = create :game, name: 'Galaxy'
+
+      games = Game.filter_by_name(value: 'galaxy', filter: true)
+      expect(games).to match_array [g]
+    end
+
+    it 'should return the best matches on top' do
+
+    end
+  end
 end
