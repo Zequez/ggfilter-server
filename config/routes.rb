1eb5f85df908eb7fbfd5c8b56fe072f9600cdb96
Ezequiel Schwartzman <zequez@gmail.com>
2016-11-24 03:39:23 -0500

    Load the FE client! Yay!
    Just a few tweaks and it’s already working! No server rendering tho,
    but yeah! Progress!

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   resources :games, only: [:index, :show]
   # resources :sysreq_tokens, only: [:index, :show, :update]
   resources :tags, only: :index
-  root to: 'games#index'
+  root to: 'app#index'
 end
